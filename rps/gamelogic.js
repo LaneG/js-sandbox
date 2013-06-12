e924a94cc3a583d4e4b093bc5949a36ef9ff755d
Lane Halley <lane@carbonfive.com>
2013-06-12 16:50:28 -0700

    this works, holding my place

--- rps/gamelogic.js
+++ rps/gamelogic.js
@@ -1,29 +1,48 @@
-var compare = function (choice1, choice2) {
+var rps=function() {
+    var userChoice = $('input[name="userChoiceInput"]').val().toLowerCase();
+
+    var computerChoice = Math.random();
+        if (computerChoice <= 0.34) {
+        computerChoice = "rock";
+            } else if(computerChoice <= 0.66) {
+            computerChoice = "paper";
+                } else {
+                computerChoice = "scissors";
+                }
+
+    // $('.result').append('<div class="item"><p>what is ' + userChoice + '? I only understand the words rock, paper and scissors.</p></div>'); 
+    $('.item').remove();
+    $('.result').append('<div class="item"><p>You picked ' + userChoice + ', I picked ' + computerChoice + '.</p></div>');
+    $('.result').append('<div class="item"><h2 class="success">' + compare(computerChoice, userChoice) + '</h2></div>');
+    return false;
+};
+
+var compare=function (choice1, choice2) {
     if (choice1 == choice2) {
         return "The result is a tie!";
     }
 
     if (choice1 === "rock") {
         if (choice2 === "scissors") {
-            return "rock wins";
+            return "rock wins.";
         } else {
-            return "paper wins";
+            return "paper wins.";
         }
     }
 
     if (choice1 === "paper") {
         if (choice2 === "rock") {
-            return "paper wins";
+            return "paper wins.";
         } else {
-            return "scissors wins";
+            return "scissors wins.";
         }
     }
 
     if (choice1 === "scissors") {
         if (choice2 === "rock") {
-            return "rock wins";
+            return "rock wins.";
         } else {
-            return "scissors wins";
+            return "scissors wins.";
         }
     }
 };
--- rps/script.js
+++ rps/script.js
@@ -1,30 +1,7 @@
 $(document).ready(function() {
 
-    var f=function() {
-        var userChoice = $('input[name="userChoiceInput"]').val().toLowerCase();
-        $('.list').append('<div class="item"><p>You pick ' + userChoice + '</p></div>');
+    $('#goButton').click(rps);
 
-        var computerChoice = Math.random();
-            if (computerChoice <= 0.34) {
-            computerChoice = "rock";
-                } else if(computerChoice <= 0.66) {
-                computerChoice = "paper";
-                    } else {
-                    computerChoice = "scissors";
-                    }
-
-        $('.item').remove();
-        $('.list').append('<div class="item"><br /><p>You picked ' + userChoice + ', I picked ' + computerChoice + '</p></div>');
-        $('.list').append('<div class="item"><h2 class="success">' + compare(computerChoice, userChoice) + '</h2></div>');
-        return false;
-    };
-
-    $('#goButton').click(f);
-
-    $('form[name="userChoiceForm"]').submit(f);
-
-    $(document).on('click','.item', function() {
-        $(this).remove();
-    });
+    $('form[name="userChoiceForm"]').submit(rps);
 
 });
