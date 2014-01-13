1d475d055627a79e1716bd93595c4597725dcaba
Lane <lane@lanehalley.com>
2014-01-12 17:26:43 -0800

    rps js split into script.js and game-logic.js

--- rps/script.js
+++ rps/script.js
@@ -1,23 +1,7 @@
 $(document).ready(function() {
-    $('#goButton').click(function() {
-        var userChoice = $('input[name="userChoiceInput"]').val();
-        $('.list').append('<div class="item"><p>You pick ' + userChoice + '</p></div>');
 
-        var computerChoice = Math.random();
-            if (computerChoice <= 0.34) {
-            computerChoice = "rock";
-                } else if(computerChoice <= 0.66) {
-                computerChoice = "paper";
-                    } else {
-                    computerChoice = "scissors";
-                    }
+    $('#goButton').click(rps);
 
-        $('.list').append('<div class="item"><p>I pick ' + computerChoice + '</p></div>');
-        $('.list').append('<div class="item"><p>' + compare(computerChoice, userChoice) + '</p></div>');
-        });
-
-    $(document).on('click','.item', function() {
-        $(this).remove();
-    });
+    $('form[name="userChoiceForm"]').submit(rps);
 
 });
