8d93e8b70bdd0d5b5833221c95f8606d819aed05
Lane Halley <lane@carbonfive.com>
2014-09-20 13:48:10 -0700

    rps: fixed compare function

--- rps/game-logic.js
+++ rps/game-logic.js
@@ -1,3 +1,6 @@
+var mystring = "hello world";
+console.log(mystring);
+
 var rps=function() {
     var userChoice = $('input[name="userChoiceInput"]').val().toLowerCase();
     if (userChoice=="") {
@@ -25,42 +28,32 @@ var rps=function() {
     return false;
 };
 
-var compare=function (choice1, choice2) {
+var compare = function (choice1, choice2) {
     if (choice1 == choice2) {
         return "The result is a tie!";
+        }
+    
+    if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            return "Rock wins.";
+         } else {
+            return "Paper wins";
+        }
     }
 
-    // if (choice1 === "rock") {
-    //     if (choice2 === "scissors") {
-    //         return "rock wins.";
-    //     } else {
-    //         return "paper wins.";
-    //         return "Rock wins.";
-    //     } else {
-    //         return "Paper wins.";
-    //     }
-    // }
-
-    // if (choice1 === "paper") {
-    //     if (choice2 === "rock") {
-    //         return "paper wins.";
-    //     } else {
-    //         return "scissors wins.";
-    //         return "Paper wins.";
-    //     } else {
-    //         return "Scissors wins.";
-    //     }
-    // }
-
-    // if (choice1 === "scissors") {
-    //     if (choice2 === "rock") {
-    //         return "rock wins.";
-    //     } else {
-    //         return "scissors wins.";
-    //         return "Rock wins.";
-    //     } else {
-    //         return "Scissors wins.";
-    //     }
-    // }
+    if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            return "Paper wins";
+         } else {
+            return "Scissors wins";
+        }
+    }
+    
+    if (choice1 === "scissors") {
+        if (choice2 === "rock") {
+            return "Rock wins";
+         } else {
+            return "Scissors wins";
+        }
+    } 
 };
-
