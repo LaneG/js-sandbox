0a9125b8adfa1f019a46eb0d9b1e685c8b402a28
Lane Halley <lane@carbonfive.com>
2014-09-20 20:16:55 -0700

    game is functional

--- rps2/game-logic.js
+++ rps2/game-logic.js
@@ -3,18 +3,6 @@ console.log(mystring);
 
 var computerOptions = ["r", "p", "s"];
 
-// var pickComputerOption = function () {
-//   var computerChoice = computerOptions[Math.floor(Math.random() * 3)];
-
-//   // console.log(computerChoice);
-//   return computerChoice;
-
-//   $('.item').remove();
-//   $('#result').prepend('<img class="item" src="images/result-rvs.jpg" />')
-//   $('#result').append('<p class="item"> ' + userChoice + ' vs. ' + computerChoice + '</p>');
-
-// };
-
 var pickComputerOption = function () {
   var computerChoice = computerOptions[Math.floor(Math.random() * 3)];
 
--- rps2/script.js
+++ rps2/script.js
@@ -16,35 +16,35 @@ $(document).ready(function(){
 
               $('.item').remove();
               $('#result').prepend('<img class="item" src=" '+ showImage +' ">');
-              $('#result').append('<p class="item"> ' + userChoice + ' vs. ' + computerChoice + '</p>');
+              // $('#result').append('<p class="item"> ' + userChoice + ' vs. ' + computerChoice + '</p>');
 
           break;
           case 'button-p':
               userChoice='p';
               var computerChoice = pickComputerOption();
-              console.log(userChoice);
-              console.log(computerChoice);
+              // console.log(userChoice);
+              // console.log(computerChoice);
 
               var showImage = "../rps2/images/result-"+userChoice+"v"+computerChoice+".jpg";
               // console.log(showImage)
 
               $('.item').remove();
-              $('#result').prepend('<img class="item" src="images/result-pvs.jpg" >')
-              $('#result').append('<p class="item"> ' + userChoice + ' vs. ' + computerChoice + '</p>');
+              $('#result').prepend('<img class="item" src=" '+ showImage +' ">');
+              // $('#result').append('<p class="item"> ' + userChoice + ' vs. ' + computerChoice + '</p>');
 
           break;
           case 'button-s':
               userChoice='s';
               var computerChoice = pickComputerOption();
-              console.log(userChoice);
-              console.log(computerChoice);
+              // console.log(userChoice);
+              // console.log(computerChoice);
 
               var showImage = "../rps2/images/result-"+userChoice+"v"+computerChoice+".jpg";
               // console.log(showImage)
 
               $('.item').remove();
-              $('#result').prepend('<img class="item" src="images/result-svs.jpg" >')
-              $('#result').append('<p class="item"> ' + userChoice + ' vs. ' + computerChoice + '</p>');
+              $('#result').prepend('<img class="item" src=" '+ showImage +' ">');
+              // $('#result').append('<p class="item"> ' + userChoice + ' vs. ' + computerChoice + '</p>');
 
           break;
       }     
