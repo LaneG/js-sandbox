da254cd8174f4474d3f07a80a663fd9633f1e94b
Lane Halley <lane@carbonfive.com>
2014-09-20 17:09:58 -0700

    button shows computerChoice

--- rps2/game-logic.js
+++ rps2/game-logic.js
@@ -1,15 +1,16 @@
 var mystring = "hello world";
 console.log(mystring);
 
-var computerOptions = ["Rock", "Paper", "Scissors"];
+var computerOptions = ["r", "p", "s"];
 
 var pickComputerOption = function () {
   var computerChoice = computerOptions[Math.floor(Math.random() * 3)];
 
   console.log(computerChoice);
-  return computerChoice;
+  // return computerChoice;
 
   $('.item').remove();
-  $('.result').append('<div class="item"><p> ' + computerChoice + '</p></div>');
-  return false; 
+  $('#result').append('<h2 class="item">' + computerChoice + '</h2>');
+
+
 };
